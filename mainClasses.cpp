7db88623d595e8ae5e3af0ea30e820fd4a8bb4c9
sjmxschm <st169819@stud.uni-stuttgart.de>
2022-10-12 19:23:30 -0400

    added mainClasses.cpp

---
+++ mainClasses.cpp
@@ -0,0 +1,18 @@
+/*
+The main classes of the program go here. The main class is
+- Player
+- Chessboard
+*/
+
+# include <string>
+using namespace std;
+
+# include "mainFunctions.cpp"
+# include "Player.cpp"
+# include "Chessboard.cpp"
+
+
+
+
+
+
